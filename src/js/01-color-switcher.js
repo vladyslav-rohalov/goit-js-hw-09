f186867371cad4b8ecb5f80aef928f15aecdba14
Vladyslav Rohalov <domus.lg@gmail.com>
2022-12-11 16:44:52 +0100

    Solution Task 1
    Solution task 1 - Switch body color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+refs.btnStop.setAttribute('disabled', 'disabled');
+
+refs.btnStart.addEventListener('click', onStartClick);
+
+function changeColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onStartClick() {
+  timerId = setInterval(changeColor, 1000);
+  refs.btnStart.setAttribute('disabled', 'disabled');
+  refs.btnStop.removeAttribute('disabled');
+}
+
+refs.btnStop.addEventListener('click', onStopClick);
+
+function onStopClick() {
+  clearInterval(timerId);
+  refs.btnStart.removeAttribute('disabled');
+  refs.btnStop.setAttribute('disabled', 'disabled');
+}
